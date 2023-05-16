d8b1da654bb1408f5fd806cfa8239561afd4b19e
marinsgui <marins0guilherme@gmail.com>
2023-05-16 11:33:41 -0300

    layour inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 function App() {
-  
 
   return (
-    <div className="App">
-      
-    </div>
+    <main className="h-screen flex justify-center items-center bg-slate-300">
+      <form className="flex flex-col justify-center gap-3 w-96 p-8 bg-white rounded">
+        <input type="text" placeholder="Email" className="border p-1" />
+        <input type="password" placeholder="Senha" className="border p-1" />
+        <button className="bg-blue-500 text-white p-2">Criar conta</button>
+      </form>
+    </main>
   )
 }
 
